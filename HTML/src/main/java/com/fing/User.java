00bd98dbfbbeaebd7c6dd5e4c67be817300f7a82
ultra <ultra@PC>
2026-02-12 19:56:14 -0600

    User.Java terminado

--- HTML/src/main/java/com/fing/User.java
+++ HTML/src/main/java/com/fing/User.java
@@ -1,5 +1,34 @@
 package com.fing;
 
+import java.util.List;
+
 public class User {
 
+    private String nombre;
+    private String correo;
+    private String github;
+    private List<String> listaCosas;
+
+    public User(String nombre, String correo, String github, List<String> listaCosas) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.github = github;
+        this.listaCosas = listaCosas;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getGithub() {
+        return github;
+    }
+
+    public List<String> getListaCosas() {
+        return listaCosas;
+    }
 }
