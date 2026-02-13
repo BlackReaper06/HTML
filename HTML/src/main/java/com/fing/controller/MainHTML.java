7e2f2e7aa46b7763f6b858592875599e969e1bd3
ultra <ultra@PC>
2026-02-12 19:48:40 -0600

    Main HTML terminado

--- HTML/src/main/java/com/fing/controller/MainHTML.java
+++ HTML/src/main/java/com/fing/controller/MainHTML.java
@@ -1,5 +1,36 @@
 package com.fing.controller;
 
+import com.fing.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class MainHTML {
 
+    @GetMapping("/usuario")
+    public User obtenerUsuario() {
+
+        List<String> cosas = new ArrayList<>();
+
+        cosas.add("Comida china");
+        cosas.add("Cyberpunk");
+        cosas.add("Mac DeMarco");
+        cosas.add("HxH");
+        cosas.add("20 Años");
+        cosas.add("Java");
+        cosas.add("Horchata");
+        cosas.add("Dormir");
+        cosas.add("Gatos");
+        cosas.add("Batman");
+
+        return new User(
+                "Erick Ramirez",
+                "ultraman060607@email.com",
+                "BlackReaper06",
+                cosas
+        );
+    }
 }
